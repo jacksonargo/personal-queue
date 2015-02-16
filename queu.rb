7554a52e0a51909e5e0a263e88dfbdaa847328ee
Jackson Argo <ignition.argo@gmail.com>
2015-02-16 14:30:18 -0500

    Added support for archiving old jobs instead of deleting them.

--- queu.rb
+++ queu.rb
@@ -4,28 +4,63 @@ require 'yaml'
 
 ## Functions
 
-# Print out a job entry
+## Get the date and time formatted as an integer
+def get_time
+    return Time.now.strftime("%Y%m%d%H%M%S").to_i
+end
+
+## Print out a job entry
 def print_job(job)
     printf "Name: %s\n", job[0]
     printf "    Summary: %s\n", job[1]["summary"]
     printf "    Priority: %i\n", job[1]["priority"]
     printf "    TTC: %i min\n", job[1]["ttc"]
+    printf "    Added: %i\n", job[1]["added"]
+    printf "    Completed: %i\n", job[1]["completed"] if job[1]["completed"] != nil
 end
 
-# Sort all jobs by priority decreasing.
+## Sort all jobs by priority decreasing.
 # Higher priority is more urgent, and lower ttc is better, so we sort by
 # priority plus inverse of ttc. Then invert everything to reverse the sort.
+# Returns an array.
 def sort_jobs(all_jobs)
-    all_jobs.sort_by { |m,k| 1.0/(k["priority"] + 1.0/k["ttc"]) }
+    return all_jobs.sort do |x,y| 
+        # First we check if the jobs have been completed
+        if x[1]["completed"] == nil and y[1]["completed"] != nil
+            -1
+        elsif x[1]["completed"] != nil and y[1]["completed"] == nil
+            1
+        # For uncomp, higher priority is more urgent, and lower ttc is better
+        elsif x[1]["completed"] == nil and y[1]["completed"] == nil
+           x_points = x[1]["priority"] + 1.0/x[1]["ttc"]
+           y_points = y[1]["priority"] + 1.0/y[1]["ttc"]
+           # Sort high to low
+           y_points <=> x_points
+        # For completed, just sort by date completed.
+        else
+           y[1]["completed"] <=> x[1]["completed"]
+        end
+    end
 end
 
-# List all jobs
-def list_jobs(all_jobs)
-    sort_jobs(all_jobs).each { |a| print_job a }
+## Print jobs to screen
+def list_jobs(all_jobs, list = nil)
+    if name == "--help"
+        puts "queu.rb list [current|completed|all]"
+        exit
+    end
+
+    # Set the default list to print
+    list = "current" if list == nil
+    # Print the list of jobs
+    sort_jobs(all_jobs).each do |a|
+        print_job a if list == "all"
+        print_job a if list == "completed" and a[1]["completed"] != nil
+        print_job a if list == "current"   and a[1]["completed"] == nil
+    end
 end
 
-
-# Insert a job into the queu.
+## Insert a job into the queu.
 # Either take the job info from options, or probe the user.
 def add_job(all_jobs, name = nil, summary = nil, priority = nil, ttc = nil)
     # Check if we need to print help
@@ -35,7 +70,7 @@ def add_job(all_jobs, name = nil, summary = nil, priority = nil, ttc = nil)
     end
 
     # Get the date; I'm not using this yet, but maybe soon.
-    date = Time.now.strftime("%Y%m%d%H%M%S").to_i
+    date = get_time
 
     # Now we'll prompt the user for any info not passed as an argument.
     if name == nil
@@ -56,13 +91,13 @@ def add_job(all_jobs, name = nil, summary = nil, priority = nil, ttc = nil)
     end
 
     # Add the job to the list
-    all_jobs[name] = {"summary" => summary, "date" => date,
+    all_jobs[name] = {"summary" => summary, "added" => date,
                          "priority" => priority.to_i, "ttc" => ttc.to_i }
     # Write the list
     write_jobs all_jobs
 end
 
-# Remove a job from the queu
+## Remove a job from the queu
 def del_job(all_jobs, name=nil)
     # Check if we need to print help
     if name == "--help"
@@ -76,11 +111,26 @@ def del_job(all_jobs, name=nil)
         name = STDIN.gets.chomp
     end
     all_jobs.delete name
+
     # Write the list
     write_jobs all_jobs
 end
 
-# Pick a job from the queu
+## Mark a job as completed or uncompleted
+def mark_job(all_jobs, name, status)
+    if name == "--help" or name == nil
+        puts "queu.rb mark NAME [completed]"
+        exit
+    end
+
+    # Store the date of completion
+    all_jobs[name]["completed"] = get_time
+
+    # Write the list
+    write_jobs all_jobs
+end
+
+## Pick a job from the queu
 def pick_job(all_jobs, algorithm = nil)
     # Sort the jobs
     sorted = sort_jobs(all_jobs)
@@ -106,7 +156,7 @@ def pick_job(all_jobs, algorithm = nil)
     end
 end
 
-# Write jobs to file
+## Write jobs to file
 def write_jobs (all_jobs)
     f = File.open($jobs_file, "w")
     f.write all_jobs.to_yaml
@@ -142,9 +192,11 @@ when "add"
 when "del"
     del_job all_jobs, ARGV[1]
 when "list"
-    list_jobs all_jobs
+    list_jobs all_jobs, ARGV[1]
 when "pick"
     pick_job all_jobs, ARGV[1]
+when "mark"
+    mark_job all_jobs, ARGV[1], ARGV[2]
 else
-    puts "queu.rb [add|del|list|pick] [OPTIONS]"
+    puts "queu.rb [add|del|mark|list|pick] [OPTIONS]"
 end
